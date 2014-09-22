064a2325b8353ba646cd1b7ee409b02ab271e368
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-09-22 18:13:41 +0900

    Add labels.

--- public/app.js
+++ public/app.js
@@ -1,14 +1,24 @@
 (function() {
-  var w = 960;
-  var h = 600;
+  var w = 1200;
+  var h = 1000;
 
   var vis = d3.select('#vis')
     .attr('width', w)
     .attr('height', h);
 
   d3.json('/graph', function(graph) {
-    var nodes = graph.nodes.map(function(name) {
-      return { label: name };
+    var nodes = graph.nodes.map(function(name, i) {
+      var isDepended = graph.links.filter(function(link) {
+        return link[1] === i;
+      }).length > 0;
+      var isDepending = graph.links.filter(function(link) {
+        return link[0] === i;
+      }).length > 0;
+      return {
+        isDepended: isDepended,
+        isDepending: isDepending,
+        label: name
+      };
     });
     var links = graph.links.map(function(link) {
       return {
@@ -21,14 +31,14 @@
   });
 
   function draw(nodes, links) {
-    console.log(nodes);
+    // Nodes
     var force = d3.layout.force()
       .size([w, h])
       .nodes(nodes)
       .links(links)
       .gravity(1)
       .linkDistance(50)
-      .charge(-500)
+      .charge(-2000)
       .linkStrength(10);
     force.start();
 
@@ -36,8 +46,7 @@
       .data(links)
     .enter()
       .append('svg:line')
-      .attr('class', 'link')
-      .attr('stroke', '#ccc');
+      .attr('class', 'link');
 
     var node = vis.selectAll('g.node')
       .data(nodes)
@@ -46,14 +55,58 @@
       .attr('class', 'node');
     node.append('svg:circle')
       .attr('r', 5)
-      .style('fill', '#555')
-      .style('stroke', '#fff')
-      .style('stroke-width', 3);
+      .attr('class', function(d) {
+        if (!d.isDepended) { return 'top-level'; }
+        if (!d.isDepending) { return 'independent'; }
+        return 'normal';
+      });
     node.call(force.drag);
+
+    // Anchors
+    var anchors = nodes.reduce(function(acc, node) {
+      acc.push({ node: node });
+      acc.push({ node: node });
+      return acc;
+    }, []);
+
+    var anchorLinks = nodes.map(function(node, i) {
+      return {
+        source: i * 2,
+        target: i * 2 + 1,
+        weight: 1
+      };
+    });
+
+    var anchorForce = d3.layout.force()
+      .nodes(anchors)
+      .links(anchorLinks)
+      .gravity(0)
+      .linkDistance(0)
+      .linkStrength(10)
+      .charge(-10)
+      .size([w, h]);
+    anchorForce.start();
+
+    var anchorNode = vis.selectAll('g.anchor-node')
+      .data(anchors)
+    .enter()
+      .append('svg:g')
+      .attr('class', 'anchor-node');
+    anchorNode.append('svg:circle')
+      .attr('r', 0)
+      .style('fill', '#fff');
+    anchorNode.append('svg:text')
+      .text(function(d, i) {
+        return i % 2 === 0 ? '' : d.node.label;
+      })
+      .style('font-size', 10);
+
+    var anchorLink = vis.selectAll('line.anchor-link')
+      .data(anchorLinks);
 
     function updateNode() {
       this.attr('transform', function(d) {
-        return 'translate(' + d.x + ',' + d.y + ')';
+        return translate(d.x, d.y);
       });
     }
 
@@ -64,10 +117,38 @@
         .attr('y2', function(d) { return d.target.y; });
     }
 
+    function translate(x, y) {
+      return 'translate(' + x + ',' + y + ')';
+    }
+
+    function layoutAnchorNodes() {
+      anchorNode.each(function(d, i) {
+        if (i % 2 === 0) {
+          d.x = d.node.x;
+          d.y = d.node.y;
+        } else {
+          var b = this.childNodes[1].getBBox();
+          var diffX = d.x - d.node.x;
+          var diffY = d.y - d.node.y;
+          var dist = Math.sqrt(diffX * diffX + diffY *diffY);
+          var shiftX = b.width * (diffX - dist) / (dist * 2);
+          shiftX = Math.max(-b.width, Math.min(0, shiftX));
+          var shiftY = 8;
+          this.childNodes[1].setAttribute('transform', translate(shiftX, shiftY));
+        }
+      });
+    }
+
     force.on('tick', function() {
-      console.log('tick');
+      anchorForce.start();
+
       node.call(updateNode);
+      anchorNode.call(updateNode);
+
+      layoutAnchorNodes();
+
       link.call(updateLink);
+      anchorLink.call(updateLink);
     });
   }
 })();
